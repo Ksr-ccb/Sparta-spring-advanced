f3f4073f335d6c78b60f70c0004cf6811924a3e6
ksr-ccb <harang4282@gmail.com>
2025-04-17 12:46:53 +0900

    feat(logger) - Lv4. add access logging to admin interceptor

---
+++ src/main/java/org/example/expert/config/LoggerInterceptor.java
@@ -0,0 +1,21 @@
+package org.example.expert.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+
+public class LoggerInterceptor implements HandlerInterceptor {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("RequestURL = {}", request.getRequestURL());
+        log.info("Request time : {}", LocalDateTime.now());
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+}
